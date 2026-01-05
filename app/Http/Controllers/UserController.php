f3feddc0cf82639f721f14d4f77008ee777f10d7
nafishaikal286-alt <nafishaikal286@gmail.com>
2026-01-06 01:46:50 +0700

    mengubag fitur resources/views/user/index.blade.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,16 @@ class UserController extends Controller
 {
     public function index()
     {
-        // Ini akan memanggil file resources/views/user/index.blade.php
-        // Pastikan file view tersebut sudah Anda buat sesuai desain Anda
-        return view('user.index'); 
+        // Data ini nantinya diambil dari database (Tabel Settings/Konfigurasi)
+        $data = [
+        // Foto Background Pilihan Admin
+        'hero_image' => 'https://images.unsplash.com/photo-1486262715619-67b85e0b08d3?q=80&w=2072&auto=format&fit=crop',
+        // Selogan Pilihan Admin
+        'slogan_title' => 'Performa Maksimal, Perjalanan Tanpa Kendala.',
+        'slogan_text' => 'Serahkan perawatan kendaraan Anda pada ahlinya. Cepat, Tepat, dan Terpercaya.'
+    ];
+
+    return view('user.index', compact('data'));
+ 
     }
 }
